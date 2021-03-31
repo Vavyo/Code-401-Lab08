b2cb174f7aa9563625ca41d3b0b937287292713f
Vavyo <vavyo2121@gmail.com>
2021-03-31 17:45:54 -0500

    first test

---
+++ LibraryClasses.tests/Classes/BookTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using LibraryClasses.Classes;
+
+namespace LibraryClasses.tests.Classes
+{
+    public class BookTests
+    {
+        [Fact]
+        public void Can_create_book()
+        {
+            //arrange
+            Book book = new Book("Moby Dick", "Herman", "Melville", 378);
+            //assert
+            Assert.NotNull(book);
+        }
+    }
+}
---
+++ LibraryClasses.tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace LibraryClasses.tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
