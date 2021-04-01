daef730a3a49c1150aaa296dfaba2b6350da7850
Vavyo <vavyo2121@gmail.com>
2021-04-01 02:14:49 -0500

    Backpack Class

---
+++ LibraryClasses/Classes/Backpack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryClasses.Classes
+{
+    public class Backpack<T> : IBag<T>
+    {
+        private List<T> contents = new List<T>();
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Pack(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Unpack(int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
