a818f2ceb69ea4d60e6457928a1d89e8b926e227
Vavyo <vavyo2121@gmail.com>
2021-04-01 01:43:41 -0500

    Borrowing test

--- LibraryClasses.tests/Classes/LibraryTests.cs
+++ LibraryClasses.tests/Classes/LibraryTests.cs
@@ -27,5 +27,21 @@ namespace LibraryClasses.tests.Classes
             // assert
             Assert.Equal(2, count);
         }
+        [Fact]
+        public void Borrowing_existing_book_returns_book_and_removes_it()
+        {
+            // arrange
+            Library library = new Library();
+            library.Add("Moby Dick", "Herman", "Melville", 378);
+            library.Add("just test title", "boring", "right?", 378);
+
+            // act
+            Book borrowed = library.Borrow("Moby Dick");
+
+            // assert
+            Assert.DoesNotContain(borrowed, library);
+            Assert.Single(library);
+
+        }
     }
 }
