7e9b1890e00d8cf7643ac88ddd18be0638d01d5d
Vavyo <vavyo2121@gmail.com>
2021-04-01 02:18:54 -0500

    Pack test

---
+++ LibraryClasses.tests/Classes/BackpackTests.cs
@@ -0,0 +1,26 @@
+﻿using LibraryClasses.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace LibraryClasses.tests.Classes
+{
+    public class BackpackTests
+    {
+        [Fact]
+        public void Pack_adds_item_to_backpack()
+        {
+            // arrange
+            Backpack<int> bag = new Backpack<int>();
+
+            // act
+            bag.Pack(3);
+
+            // assert
+            Assert.Contains(3, bag);
+        }
+    }
+}
